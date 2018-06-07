2b221679ce70efe22f9305863c7b0d44376e23ff
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-06-08 00:40:16 +0800

    fix login interface, add profile tab to nav bar

--- skillshot/client/main.js
+++ skillshot/client/main.js
@@ -1,6 +1,29 @@
 import { Accounts } from "meteor/accounts-base";
+import { Template } from "meteor/templating";
+import { Notes } from "../lib/collection.js";
+
+import "./main.html";
+
+//This extract the data of login user from database
+export const Profiles = new Mongo.Collection("profiles");
 
 // Accounts config
 Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
+  passwordSignupFields: "USERNAME_AND_EMAIL"
+});
+
+Accounts.sendResetPasswordEmail(userId, [email], [extraTokenData]);
+
+Template.body.helpers({
+  projects() {
+    return Projects.find({});
+  }
+});
+
+$(function() {
+  $(".nav li a").click(function(e) {
+    e.preventDefault(); //To prevent the default anchor tag behaviour
+    var url = this.href;
+    $(".main").load(url);
+  });
 });
