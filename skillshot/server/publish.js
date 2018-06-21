6f2483efd4cf2e48cc64f0998b6afecb9de0510c
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-06-21 11:42:53 +0800

    resolve merge conflict

--- skillshot/server/publish.js
+++ skillshot/server/publish.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 Meteor.publish("projects", function() {
   return Projects.find({ leader: this.userId });
-=======
-Meteor.publish('projects', function(){
-  return Projects.find({author: this.userId});
->>>>>>> 338edcf78b687bc88288297d5359f59d0b260690
 });
