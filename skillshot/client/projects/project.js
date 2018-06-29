5a4564b634ac40642f6faf5ad1284672c73730d8
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-06-29 18:53:31 +0800

    edit some styling with fa awesome

--- skillshot/client/projects/project.js
+++ skillshot/client/projects/project.js
@@ -1,3 +1,5 @@
+import { Session } from "meteor/session";
+
 Template.browse.onCreated(function() {
   var self = this;
   self.autorun(function() {
@@ -10,13 +12,17 @@ Template.browse.helpers({
     return Projects.find({});
   }
 });
+Session.set("editMode", false);
 
 Template.Project.events({
   "click .toggle-menu": function() {
     Meteor.call("toggleMenuItem", this._id, this.inProject);
   },
-  "click .fa-trash": function() {
+  "click .fa-trash-alt": function() {
     Meteor.call("deleteProject", this._id);
+  },
+  "click .fa-edit": function() {
+    Session.set("editMode", !Session.get("editMode"));
   }
 });
 
