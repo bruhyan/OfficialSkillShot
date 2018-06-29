d5bf0e44ca8d0ac2b26e1ed8f90afff107c1af3e
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-06-29 15:42:43 +0800

    fix issue of all project not appearing in profile page

--- skillshot/client/profile/profile.js
+++ skillshot/client/profile/profile.js
@@ -1,17 +1,32 @@
+Template.profile.onCreated(function() {
+  var self = this;
+  self.autorun(function() {
+    self.subscribe("projects");
+  });
+});
+
 Template.profile.helpers({
   email: function() {
-    if(!Meteor.user()) {
-      Bert.alert("you are not logged in, permission denied", "danger", "growl-top-right");
+    if (!Meteor.user()) {
+      Bert.alert(
+        "you are not logged in, permission denied",
+        "danger",
+        "growl-top-right"
+      );
       return false;
-    }else {
+    } else {
       return Meteor.user().emails[0].address;
     }
   },
   username: function() {
-    if(!Meteor.user()) {
-      Bert.alert("you are not logged in, permission denied", "danger", "growl-top-right");
+    if (!Meteor.user()) {
+      Bert.alert(
+        "you are not logged in, permission denied",
+        "danger",
+        "growl-top-right"
+      );
       return false;
-    }else {
+    } else {
       return Meteor.user().username; //username will only be displayed if we switch over to custom user registration
     }
   },
@@ -19,7 +34,10 @@ Template.profile.helpers({
   userProjects: function() {
     var username = Meteor.user().username;
     var userId = Meteor.userId();
-    var userProjects = Projects.find({userId: userId}, {sort : {createdAt: -1}});
+    var userProjects = Projects.find(
+      { userId: userId },
+      { sort: { createdAt: -1 } }
+    );
     return userProjects;
   }
 });
