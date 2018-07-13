bf794b5e5e0846baea248212dc3e3febff6ab163
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-07-14 01:47:15 +0800

    -

--- skillshot/server/publish.js
+++ skillshot/server/publish.js
@@ -6,3 +6,16 @@ Meteor.publish("singleProject", function(id) {
   check(id, String);
   return Projects.find({ _id: id });
 });
+
+Meteor.publish("userData", function() {
+  if (this.userId) {
+    return Meteor.users.find(
+      { _id: this.userId },
+      {
+        fields: { other: 1, things: 1 }
+      }
+    );
+  } else {
+    this.ready();
+  }
+});
