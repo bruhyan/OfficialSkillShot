192f9d9b384c440f8ba3bb012250fa74183212cf
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-07-14 01:46:59 +0800

    fix edit issue in browse add jetsetter package to monitor session varibles

--- skillshot/client/projects/project.js
+++ skillshot/client/projects/project.js
@@ -12,40 +12,42 @@ Template.browse.helpers({
     return Projects.find({});
   }
 });
-Session.set("editMode", false);
 
-Template.Project.events({
-  "click .toggle-menu": function() {
-    Meteor.call("toggleMenuItem", this._id, this.inProject);
-    if (this.inProject == false) {
-    Bert.alert(
-      "Project added to profile subscriptions",
-      "success",
-      "growl-top-right"
-    );
-  } else {
-    Bert.alert(
-      "Project removed from profile subscriptions",
-      "danger",
-      "growl-top-right"
-    );
-  }
-  },
-  "click .fa-trash-alt": function() {
-    Meteor.call("deleteProject", this._id);
-    Bert.alert(
-      "Project Deleted",
-      "danger",
-      "growl-top-right"
-    );
-  },
-  "click .fa-edit": function() {
-    Session.set("editMode", !Session.get("editMode"));
-  }
+Template.Project.onCreated(function() {
+  this.editMode = new ReactiveVar(false);
 });
 
 Template.Project.helpers({
   updateProjectId: function() {
     return this._id;
+  },
+  editMode: function() {
+    return Template.instance().editMode.get();
+  }
+});
+
+Template.Project.events({
+  "click .toggle-menu": function() {
+    Meteor.call("toggleMenuItem", this._id, this.inProject);
+    if (this.inProject == false) {
+      Bert.alert(
+        "Project added to profile subscriptions",
+        "success",
+        "growl-top-right"
+      );
+    } else {
+      Bert.alert(
+        "Project removed from profile subscriptions",
+        "danger",
+        "growl-top-right"
+      );
+    }
+  },
+  "click .fa-edit": function(event, template) {
+    template.editMode.set(!template.editMode.get());
+  },
+  "click .fa-trash-alt": function() {
+    Meteor.call("deleteProject", this._id);
+    Bert.alert("Project Deleted", "danger", "growl-top-right");
   }
 });
