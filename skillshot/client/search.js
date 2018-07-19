f2cfea5ee497e2a09fad6848e7654dc950d67fb3
DESKTOP-T5T7LS5\Zhi Kuan <xiaoichiban@hotmail.com>
2018-07-19 18:00:10 +0800

    Now notification shown when add project
    *Issue: Project didnt add to user list of project

--- skillshot/client/search.js
+++ skillshot/client/search.js
@@ -7,23 +7,24 @@ Template.search.onCreated(function() {
   });
 });
 
-
 Template.search.helpers({
   inputAttributes: function() {
-    return {'class': 'easy-search-input', 'placeholder': 'Start Searching'};
+    return { class: "easy-search-input", placeholder: "Start Searching" };
   },
   players: function() {
-    return Projects.find({}, {sort: {createdAt: -1} });
+    return Projects.find({}, { sort: { createdAt: -1 } });
   },
   selectedName: function() {
-    var project = ProjectsIndex.config.mongoCollection.findOne({__originalId: Session.get("selectedProject") });
+    var project = ProjectsIndex.config.mongoCollection.findOne({
+      __originalId: Session.get("selectedProject")
+    });
     return project && project.name;
   },
   index: function() {
     return ProjectsIndex;
   },
   resultsCount: function() {
-    return ProjectsIndex.getComponentDict().get('count');
+    return ProjectsIndex.getComponentDict().get("count");
   },
   showMore: function() {
     return false;
@@ -33,41 +34,50 @@ Template.search.helpers({
   //integrating browse function
   projects: () => {
     return Projects.find({});
-  },
-
+  }
 });
 
 Session.set("editMode", false);
 
 Template.User.helpers({
   selected: function() {
-    return Session.equals("selectedProject", this.__originalId) ? "selected" : '';
+    return Session.equals("selectedProject", this.__originalId)
+      ? "selected"
+      : "";
   },
 
-//integrating browse functions
+  //integrating browse functions
   updateProjectId: function() {
     return this._id;
   }
-
-
 });
 
 Template.User.events({
-  'click' : function() {
+  click: function() {
     Session.set("selectedProject", this.__originalId);
   },
 
-  //integrating browse functions:
   "click .toggle-menu": function() {
     Meteor.call("toggleMenuItem", this._id, this.inProject);
+    if (this.inProject == false) {
+      Bert.alert(
+        "Project added to profile subscriptions",
+        "success",
+        "growl-top-right"
+      );
+    } else {
+      Bert.alert(
+        "Project removed from profile subscriptions",
+        "danger",
+        "growl-top-right"
+      );
+    }
   },
+
+  //integrating browse functions:
   "click .fa-trash-alt": function() {
     Meteor.call("deleteProject", this._id);
-    Bert.alert(
-      "Project Deleted",
-      "danger",
-      "growl-top-right"
-    );
+    Bert.alert("Project Deleted", "danger", "growl-top-right");
   },
   "click .fa-edit": function() {
     Session.set("editMode", !Session.get("editMode"));
